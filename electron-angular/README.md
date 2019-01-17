5242058d7102a0f027dfff73dc8e9935c220b1ec
Alejandro Almazán Cabo <aalmazan@autentia.com>
2019-01-18 00:33:05 +0100

    Add Electron Angular App

---
+++ electron-angular/README.md
@@ -0,0 +1,7 @@
+# Electron Angular App
+
+```
+npm install
+npm run electron
+npm run package
+```
---
+++ electron-angular/src/electron-main.ts
@@ -0,0 +1,51 @@
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+import * as url from 'url';
+
+let mainWindow: Electron.BrowserWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1281,
+        height: 800,
+        minWidth: 1281,
+        minHeight: 800,
+        backgroundColor: '#3ecf7d',
+        icon: path.join(__dirname, 'electron-angular/assets/app_icon.png'),
+        webPreferences: {
+            nodeIntegration: false,
+            allowRunningInsecureContent: false,
+            webSecurity: true
+        }
+    });
+
+    // and load the index.html of the app.
+    mainWindow.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'electron-angular/index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
+
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
